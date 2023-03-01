cff867298ada5adc520d782d1a6a2e9b87b33a78
Trung Hiếu <106734395+HMadd64@users.noreply.github.com>
2023-03-01 09:45:29 +0700

    end

---
+++ nhom_Mesa_CC/Nhom_Mesa_CC.cs
@@ -0,0 +1,29 @@
+﻿namespace nhom_Mesa_CC
+{
+    public partial class Nhom_Mesa_CC : Form
+    {
+        public Nhom_Mesa_CC()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text.ToString().Equals("admin") && txtpassword.Text.ToString().Equals("admin"))
+            {
+                //MessageBox.Show("Đăng nhập thành công");
+                //this.Close();
+                Success f = new Success();
+                f.Show();
+            }else
+            {
+                MessageBox.Show("Đăng nhập k thành công");
+            }    
+        }
+    }
+}
---
+++ nhom_Mesa_CC/Success.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace nhom_Mesa_CC
+{
+    partial class Success
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(285, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đăng nhập thành công rồi á chàiiiii!!!";
+            // 
+            // Success
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "Success";
+            this.Text = "Success";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
